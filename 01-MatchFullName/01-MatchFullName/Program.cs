e8f23372da51bf633ff43ba337137ccf1825a5ef
denisYordanov-eng <denisivov437@gmail.com>
2025-06-01 06:22:47 +0300

    Made a task using Regex.

---
+++ 01-MatchFullName/01-MatchFullName/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01_MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = Console.ReadLine();
+            string matchPatern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b";
+            
+            Regex regex = new Regex(matchPatern);
+            MatchCollection match = regex.Matches(words);
+
+        
+
+            foreach (Match m in match)
+            {
+              Console.Write(m.ToString() + " ");
+            }
+           
+        }
+    }
+}
