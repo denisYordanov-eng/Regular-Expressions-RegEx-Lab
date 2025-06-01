ffbb1e43493ecb27f91112e5572f673cd35b7098
denisYordanov-eng <denisivov437@gmail.com>
2025-06-01 06:35:10 +0300

    Made the first task using regex and string builder.

---
+++ 01-MatchFullName-StringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01_MatchFullName_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            string words = Console.ReadLine();
+            var patern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b";
+
+            Regex regex = new Regex(patern);
+            MatchCollection matches = regex.Matches(words);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (Match m in regex.Matches(words))
+            {
+                sb.Append(m.ToString()).Append(" "); 
+            }
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
