672b6b8efa9ee12406d47f79ce3e6fc3abfc7aa3
denisYordanov-eng <denisivov437@gmail.com>
2025-06-01 12:52:05 +0300

    Made a task.

---
+++ 03-MatchHexadecimalNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03_MatchHexadecimalNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string matchPatern = @"\b(?:0X|0x)?([0-9A-Fa-f]+)\b";
+
+            string numberAsString = Console.ReadLine();
+
+            var Hexnumbers = Regex.Matches(numberAsString,matchPatern)
+                .Cast<Match>()
+                .Select(a =>a.Value)
+                .ToArray();
+
+            Console.WriteLine(String.Join(" ", Hexnumbers));
+        }
+    }
+}
