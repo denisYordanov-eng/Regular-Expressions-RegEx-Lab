eb69fde0c51fd1a09c2ed74c7228e8cd497dd28c
denisYordanov-eng <denisivov437@gmail.com>
2025-06-02 06:41:24 +0300

    Made a task.

---
+++ 06-Replace-Tag/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _06_Replace_Tag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input !="end")
+            {
+                var patern = @"<a\s+href\s*=\s*""(.*?)"">(.*?)</a>";
+                string replacement = @"[URL href=""$1""]$2[/URL]";
+                var result = Regex.Replace(input,patern,replacement);
+                Console.WriteLine(result);
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
