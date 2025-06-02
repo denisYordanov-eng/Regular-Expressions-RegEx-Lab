0ba9ab6bd563612778231d78968394d4bb80b528
denisYordanov-eng <denisivov437@gmail.com>
2025-06-02 06:12:45 +0300

    Made a task.

---
+++ 05-MatchNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace _05_MatchNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+
+            string input = Console.ReadLine();
+            var numbersMatch = Regex.Matches(input, regex);
+
+            foreach (Match number in numbersMatch)
+            {
+                Console.Write(number.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
